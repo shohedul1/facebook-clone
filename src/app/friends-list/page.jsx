92c482c53f8a27d12aeb0e5a033e328cb754d62e
Jane Doe <jane.doe@example.com>
2024-10-17 00:00:00 +0600

    secound-commit

--- src/app/friends-list/page.jsx
+++ src/app/friends-list/page.jsx
@@ -1,27 +1,22 @@
 'use client';
-import React, { useState } from 'react'
-import LeftSideBar from '../components/LeftSideBar'
-import { FriendCardSkeleton, NoFriendsMessage } from '@/lib/Skeleten'
+import React, { useState } from 'react';
+import LeftSideBar from '../components/LeftSideBar';
+import { FriendCardSkeleton, NoFriendsMessage } from '@/lib/Skeleten';
 import FriendRequest from './FriendRequest';
 import FriendsSuggestion from './FriendsSuggestion';
 
-const page = () => {
+const Page = () => {
     const [loading, setLoading] = useState(false);
 
-    const friendRequest = [{
-
-    }];
-
-    const friendSuggestion = [{
-
-    }];
+    const friendRequest = [{}];
+    const friendSuggestion = [{}];
 
     return (
-        <div className="min-h-screen bg-gray-100 dark:bg-[rgb(36,37,38)] ">
+        <div className="min-h-screen bg-gray-100 dark:bg-[rgb(36,37,38)]">
             <LeftSideBar />
             <main className="ml-0 md:ml-64 mt-16 p-6">
                 <h1 className="text-2xl font-bold mb-6">Friends Requests</h1>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6  ">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     {loading ? (
                         <FriendCardSkeleton />
                     ) : friendRequest.length === 0 ? (
@@ -30,12 +25,14 @@ const page = () => {
                             description="Looks like you are all caught up! Why not explore and connect with new people?"
                         />
                     ) : (
-                        friendRequest.map((friend, index) => <FriendRequest key={index} friend={friend} />)
+                        friendRequest.map((friend, index) => (
+                            <FriendRequest key={index} friend={friend} />
+                        ))
                     )}
                 </div>
 
                 <h1 className="text-2xl font-bold mb-6">People you may know</h1>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6  ">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     {loading ? (
                         <FriendCardSkeleton />
                     ) : friendSuggestion.length === 0 ? (
@@ -51,7 +48,7 @@ const page = () => {
                 </div>
             </main>
         </div>
-    )
-}
+    );
+};
 
-export default page
+export default Page;
